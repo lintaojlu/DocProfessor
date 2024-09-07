c40790f3e4a40b991848fca9b14b02f3b60ef456
lintaojlu <lintaojlu@qq.com>
2024-09-07 19:51:07 +0800

    [update] complete the api interface

--- web_test_flask.py
+++ web_test_flask.py
@@ -3,11 +3,6 @@ import logging
 import json
 import os
 
-from doc_chat import DocAssistant
-from doc_study import DocStudy
-from utils.embedding import LocalEmbedding
-from utils.llm_api import get_model_answer
-from utils.utils import get_doc_content
 from doc_professor import DocProfessor
 from werkzeug.utils import secure_filename
 
@@ -18,11 +13,15 @@ logging.basicConfig(level=logging.DEBUG)
 professor = DocProfessor(user_dir='user_data/lintao/', model='gpt-4o-2024-05-13')
 professor.load_categories_map()
 
-#TODO 根据前端设计接口
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+"""
+文档管理功能
+"""
 
 
 @app.route('/upload_files', methods=['POST'])
@@ -59,23 +58,6 @@ def upload_files():
 def resolve_files():
     file_status = professor.resolve_docs()
     return jsonify(file_status)
-
-
-@app.route('/answer', methods=['POST'])
-def generate_response_stream():
-    input_text = request.json.get('input_text')
-
-    def generate():
-        title = professor.assistant.generate_conversation_title(input_text)
-        # 先返回标题
-        yield f"data: {json.dumps({'title': title})}\n\n"
-
-        # 然后流式生成回答
-        response_stream = professor.assistant.answer_question_with_knowledge(input_text)
-        for chunk in response_stream:
-            yield f"data: {json.dumps({'answer_chunk': chunk})}\n\n"
-
-    return Response(generate(), mimetype='text/event-stream')
 
 
 @app.route('/get_categories', methods=['GET'])
@@ -120,100 +102,76 @@ def update_category():
 @app.route('/view_file_details', methods=['GET'])
 def view_file_details():
     # 功能: 查看文档解析细节
-    # 参数: 文件名或ID
-    pass
+    # 参数: 文件名
+    file_name = request.args.get('file_name')
+
+    if not file_name:
+        return jsonify({"error": "请提供文件名或ID"}), 400
+
+    details = professor.get_resolve_result(file_name)
+
+    return jsonify({"details": details})
 
 
 @app.route('/delete_file', methods=['POST'])
 def delete_file():
     # 功能: 删除文档
-    # 参数: 文件名或ID
-    pass
+    # 参数: 文件名
+    file_name = request.json.get('file_name')
+
+    if not file_name:
+        return jsonify({"error": "请提供文件名或ID"}), 400
+
+    professor.delete_file(file_name)
+
+    return jsonify({"message": "文件删除成功"})
 
 
-@app.route('/edit_tag', methods=['POST'])
-def edit_tag():
-    # 功能: 编辑标签内容
-    # 参数: 文件名或ID, 标签内容
-    pass
+@app.route('/get_doc_table', methods=['GET'])
+def get_all_docs():
+    # 功能: 获取所有的文档信息
+    # 参数: 无
+    # format [{'doc_name': '', 'category': '', 'keywords': '', 'doc_path': '', 'summary_path': '', 'info_path': ''}]
+    doc_table = professor.get_all_docs()
+    return jsonify(doc_table)
 
-
-@app.route('/get_page_content', methods=['GET'])
-def get_page_content():
-    # 功能: 访问相应页内容
-    # 参数: 页码, 每页项目数量
-    pass
 
 @app.route('/get_documents_by_category', methods=['GET'])
 def get_documents_by_category():
     # 功能: 查看某分类下的全部文档
-    # 参数: 分类ID或名称
-    pass
+    # 参数: 分类名称
+    category = request.args.get('category')
+    if not category:
+        return jsonify({"error": "请提供分类名称"}), 400
+
+    doc_table = professor.get_docs_by_category(category)
+    return jsonify(doc_table)
 
 
-@app.route('/get_subcategories', methods=['GET'])
-def get_subcategories():
-    # 功能: 查看一级分类下的二级分类文件夹
-    # 参数: 一级分类ID或名称
-    pass
+"""
+知识库问答功能
+"""
 
 
-@app.route('/document_management', methods=['GET'])
-def document_management():
-    # 功能: 进入文档管理界面
-    pass
+@app.route('/answer', methods=['POST'])
+def generate_response_stream():
+    input_text = request.json.get('input_text')
 
+    def generate():
+        title = professor.assistant.generate_conversation_title(input_text)
+        # 返回标题
+        yield f"data: {json.dumps({'title': title})}\n\n"
 
-@app.route('/knowledge_base_qa', methods=['GET'])
-def knowledge_base_qa():
-    # 功能: 进入知识库问答功能界面
-    pass
+        # 检索相关知识
+        knowledge = professor.assistant.get_related_knowledge(input_text)
+        yield f"data: {json.dumps({'related_knowledge': knowledge})}\n\n"
 
+        # 流式生成回答
+        response_stream = professor.assistant.generate_response_stream(input_text, knowledge)
+        for chunk in response_stream:
+            yield f"data: {json.dumps({'answer_chunk': chunk})}\n\n"
 
-@app.route('/knowledge_graph', methods=['GET'])
-def knowledge_graph():
-    # 功能: 进入知识图谱功能界面
-    pass
-
-
-@app.route('/view_conversation/<conversation_id>', methods=['GET'])
-def view_conversation(conversation_id):
-    # 功能: 查看历史问答的某个对话
-    # 参数: conversation_id (会话ID)
-    pass
-
-
-@app.route('/create_conversation', methods=['POST'])
-def create_conversation():
-    # 功能: 新建会话
-    pass
-
-
-@app.route('/send_message', methods=['POST'])
-def send_message():
-    # 功能: 发送对话
-    # 参数: 输入的对话内容
-    pass
-
-
-@app.route('/view_related_document/<message_id>', methods=['GET'])
-def view_related_document(message_id):
-    # 功能: 查看与回答相关联的文档
-    # 参数: message_id (消息ID)
-    pass
-
-
-@app.route('/highlight_related_articles', methods=['POST'])
-def highlight_related_articles():
-    # 功能: 知识图谱中高亮显示与某圆圈直接关联的文章
-    # 参数: 节点ID或文章ID
-    pass
-
-
-@app.route('/login', methods=['POST'])
-def login():
-    # 功能: 用户登录
-    pass
+    return Response(generate(), mimetype='text/event-stream')
 
 
 if __name__ == '__main__':
